e58aef76642b8603b4abf3af314beb4c7b4923df
Student246752 <246752@student.pwr.edu.pl>
2021-05-17 18:44:22 +0200

    Add authentication with scopes

--- Rent4Events/urls.py
+++ Rent4Events/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('users/', views.user_list),
     path('users/<int:pk>/', views.user_detail),
     path('groups/', views.group_list),
+    path('api/public', views.public),
+    path('api/private', views.private),
+    path('api/private-scoped', views.private_scoped),
     # path('groups/<int:pk>/', views.user_detail),
 ]
--- Rent4Events/views.py
+++ Rent4Events/views.py
@@ -3,17 +3,75 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-
+from rest_framework.decorators import api_view, permission_classes
 
 import django.contrib.auth.models as auth
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import AllowAny
 
 from Rent4Events.models import Product, OrderPosition
 # from Rent4Events.serializers import UserSerializer, GroupSerializer, ProductSerializer
 from Rent4Events.serializers import *
+from functools import wraps
+import jwt
+
+from django.http import JsonResponse
+
+def get_token_auth_header(request):
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.META.get("HTTP_AUTHORIZATION", None)
+    parts = auth.split()
+    token = parts[1]
+    print(auth)
+    return token
+
+def requires_scope(required_scope):
+    """Determines if the required scope is present in the Access Token
+    Args:
+        required_scope (str): The scope required to access the resource
+    """
+    def require_scope(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = get_token_auth_header(args[0])
+            decoded = jwt.decode(token, verify=False)
+            print('decoded:', decoded)
+            if decoded.get("permissions"):
+                token_scopes = decoded["permissions"]
+                for token_scope in token_scopes:
+                    print('scope:', token_scope)
+                    if token_scope == required_scope:
+                        return f(*args, **kwargs)
+            response = JsonResponse({'message': 'You don\'t have access to this resource'})
+            response.status_code = 403
+            return response
+        return decorated
+    return require_scope
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def public(request):
+    return JsonResponse({'message': 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'})
+
+
+@api_view(['GET'])
+def private(request):
+    return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated to see this.'})
+
+
+@api_view(['GET'])
+@requires_scope('read:messages')
+def private_scoped(request):
+    return JsonResponse({'message': 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'})
+
+
+"""
+TESTING
+"""
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -21,7 +79,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = auth.User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -169,11 +227,11 @@ def group_list(request):
         return JsonResponse(serializer.errors, status=400)
 
 
-@api_view(['GET'])
-def public(request):
-    return HttpResponse("You don't need to be authenticated to see this")
-
-
-@api_view(['GET'])
-def private(request):
-    return HttpResponse("You should not see this message if not authenticated!")
+# @api_view(['GET'])
+# def public(request):
+#     return HttpResponse("You don't need to be authenticated to see this")
+#
+#
+# @api_view(['GET'])
+# def private(request):
+#     return HttpResponse("You should not see this message if not authenticated!")
