f9bf92c8553fdc84bdb661582c05ac288f373547
Student246752 <246752@student.pwr.edu.pl>
2021-07-01 17:54:20 +0200

    Fix css -> !important

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,59 +24,59 @@ export default {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: var(--COLOR1);
+  color: var(--COLOR1)!important;
 }
 
 .btn-1, .btn-outline-1:hover, .btn-1:focus {
-    color: #fff;
-    background-color: var(--COLOR1);
-    border-color: var(--COLOR1);
+    color: #fff!important;
+    background-color: var(--COLOR1)!important;
+    border-color: var(--COLOR1)!important;
 }
 
 .btn-1:hover, .btn-outline-1:focus, .btn-outline-1 {
-    color: var(--COLOR1);
-    border-color: var(--COLOR1);
-    background-color: #FFFFFF;
+    color: var(--COLOR1)!important;
+    border-color: var(--COLOR1)!important;
+    background-color: #FFFFFF!important;
 }
 
 .btn-2, .btn-outline-2:hover, .btn-2:focus {
-    color: #fff;
-    background-color: var(--COLOR2);
-    border-color: var(--COLOR2);
+    color: #fff!important;
+    background-color: var(--COLOR2)!important;
+    border-color: var(--COLOR2)!important;
 }
 
 .btn-2:hover, .btn-outline-2:focus, .btn-outline-2 {
-    color: var(--COLOR2);
-    border-color: var(--COLOR2);
-    background-color: #FFFFFF;
+    color: var(--COLOR2)!important;
+    border-color: var(--COLOR2)!important;
+    background-color: #FFFFFF!important;
 }
 
 .btn-3, .btn-outline-3:hover, .btn-3:focus {
-    color: #fff;
-    background-color: var(--COLOR3);
-    border-color: var(--COLOR3);
+    color: #fff!important;
+    background-color: var(--COLOR3)!important;
+    border-color: var(--COLOR3)!important;
 }
 
 .btn-3:hover, .btn-outline-3:focus, .btn-outline-3{
-    color: var(--COLOR3);
-    border-color: var(--COLOR3);
-    background-color: #FFFFFF;
+    color: var(--COLOR3)!important;
+    border-color: var(--COLOR3)!important;
+    background-color: #FFFFFF!important;
 }
 
 .btn-4, .btn-outline-4:hover, .btn-4:focus {
-    color: #fff;
-    background-color: var(--COLOR4);
-    border-color: var(--COLOR4);
+    color: #fff!important;
+    background-color: var(--COLOR4)!important;
+    border-color: var(--COLOR4)!important;
 }
 
 .btn-4:hover, .btn-outline-4:focus, .btn-outline-4 {
-    color: var(--COLOR4);
-    border-color: var(--COLOR4);
-    background-color: #FFFFFF;
+    color: var(--COLOR4)!important;
+    border-color: var(--COLOR4)!important;
+    background-color: #FFFFFF!important;
 }
 
 .btn {
-    border-radius: 0;
+    border-radius: 0!important;
 }
 
 </style>
